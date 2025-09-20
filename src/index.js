dfad37def5a05a2bbd97f57f6054fa3f1df5696d
HassanAbbas10 <hassanabbas05674@gmail.com>
2025-09-20 13:14:34 +0500

    winston logger working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import logger from "./config/logger.js";
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(express(urlencoded({ extended: true })));
+app.use(
+	morgan("combined", {
+		stream: { write: (message) => logger.info(message.trim()) },
+	}),
+);
 
 const PORT = 3000;
 
